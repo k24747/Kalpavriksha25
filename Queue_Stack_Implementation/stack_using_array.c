2cde3e0f045f1de619f68f79caf05d9b20511f00
Kavya Khandelwal <kavyakhandelwal300@gmail.com>
2025-01-31 11:37:05 +0000

    Implementation of stack using array

---
+++ Queue_Stack_Implementation/stack_using_array.c
@@ -0,0 +1,94 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct stack
+{
+    int* arr;
+    int top;
+    int size;
+}stack;
+
+int isEmpty(stack* st)
+{
+    if(st->top == -1)
+    return 1;
+
+    return 0;
+}
+
+stack* push(stack* st)
+{
+    if(st->top == st->size)
+    {
+        st->size += st->size;
+        st->arr = (int*)realloc(st->arr,sizeof(int)*st->size);
+    }
+    int value;
+    scanf("%d",&value);
+    st->arr[st->top+1] = value;
+    st->top++;
+    return st; 
+}
+
+void pop(stack* st)
+{
+    if(isEmpty(st) == 1)
+    {
+        printf("Stack Underflow\n");
+        return;
+    }
+    st->top--;
+}
+
+void peek(stack* st)
+{
+    if(st->top == -1)
+    {
+        printf("Stack is Empty\n");
+        return ;
+    }
+    printf("%d\n",st->arr[st->top]);
+}
+
+void size(stack* st)
+{
+    printf("Size of Stack is : %d\n",st->top+1);
+}
+
+int main()
+{
+    stack* st = (stack*)malloc(sizeof(stack));
+    st->top = -1;
+    st->size = 100;
+    st->arr = (int*)malloc(100*sizeof(int));
+    while(1)
+    {
+        int choice;
+        printf("\nEnter the operation.\n1.Push\n2.Pop\n3.Peek\n4.isEmpty\n5.Size\n\n");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            st = push(st);
+            break;
+            case 2:
+            pop(st);
+            break;
+            case 3:
+            peek(st);
+            break;
+            case 4:
+            if(isEmpty(st) == 1)
+            printf("Stack is Empty\n");
+            else
+            printf("Stack is Not Empty\n");
+            break;
+            case 5:
+            size(st);
+            break;
+            default:
+            printf("Enter Valid Input");
+        }
+    }
+    return 0;
+}
